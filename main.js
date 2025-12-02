d70f037c776eff2ac1fd29626f3f987530b3996f
Denis-7242 <dexdenis3@gmail.com>
2025-12-02 22:12:49 +0300

    updated some codes on the DOM manipulation

---
+++ main.js
@@ -0,0 +1,25 @@
+const rootDiv = document.getElementById('root');
+
+const heading = document.createElement('h1');
+
+heading.innerText = "Welcome to Dex'N.com";
+
+rootDiv.appendChild(heading);
+
+const colors = ['red', 'blue', 'green',]
+
+colors.forEach((c) => {
+    renderButtons(c);
+})
+
+function renderButtons(color) {
+    const button = document.createElement('button');
+    button.innerText = color;
+    button.style.backgroundColor = color;
+    button.style.marginRight = "20px";
+
+    button.addEventListener("click", () => {
+        heading.style.color = color;
+    }); 
+    rootDiv.appendChild(button);
+}
