752133cac6ff36b9505d8b5def07673e0501f321
Denis-7242 <dexdenis3@gmail.com>
2025-12-07 20:26:08 +0300

    formatted the js file codes

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-const rootDiv = document.getElementById('root');
+const rootDiv = document.getElementById("root");
 
-const heading = document.createElement('h1');
+const heading = document.createElement("h1");
 
 heading.innerText = "Welcome to Dex'N.com";
 
 rootDiv.appendChild(heading);
 
-const colors = ['red', 'blue', 'green',]
+const colors = ["red", "blue", "green"];
 
 colors.forEach((c) => {
-    renderButtons(c);
-})
+  renderButtons(c);
+});
 
 function renderButtons(color) {
-    const button = document.createElement('button');
-    button.innerText = color;
-    button.style.backgroundColor = color;
-    button.style.marginRight = "20px";
+  const button = document.createElement("button");
+  button.innerText = color;
+  button.style.backgroundColor = color;
+  button.style.marginRight = "20px";
 
-    button.addEventListener("click", () => {
-        heading.style.color = color;
-    }); 
-    rootDiv.appendChild(button);
+  button.addEventListener("click", () => {
+    heading.style.color = color;
+  });
+  rootDiv.appendChild(button);
 }
